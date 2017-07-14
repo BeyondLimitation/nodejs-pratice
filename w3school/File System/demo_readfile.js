e2580e45c2afcdb1b8d66bc6776fd453f6347f9c
heechan Lee <leeheechan1@gmail.com>
2017-07-14 17:02:14 +0900

    한글 출력을 위한 utf-8 설정

--- w3school/File System/demo_readfile.js
+++ w3school/File System/demo_readfile.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
   fs.readFile(__dirname+'/demofile1.html', 'utf-8', function (err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
     res.write(data);
   });
 }).listen(8080);
