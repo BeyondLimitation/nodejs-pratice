38b81e0796695805cb4a63089a9651c4f6a893f8
heechan Lee <leeheechan1@gmail.com>
2017-07-14 16:35:59 +0900

    출력이 재대로 되게함.

--- w3school/File System/demo_readfile.js
+++ w3school/File System/demo_readfile.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', 'utf-8', (err, data) => {
+  fs.readFile(__dirname+'/demofile1.html', 'utf-8', function (err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-    return res.end();
   });
 }).listen(8080);
+console.log('program started');
