bf660e1e1c0a1e8bbef93cec3e27281862f6a912
BeyondLimitation <a_line1@naver.com>
2017-07-14 11:57:15 +0900

    내 Node.js 연습
    Node.js연습용 저장소

---
+++ w3school/File System/demo_fs_append.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+fs.appendFile('./mynewFile1.txt', '안녕 컨텐츠?', (err) =>{
+  if (err) {
+    throw err;
+  }
+  console.log("저장됨!");
+});
---
+++ w3school/HTTP module/demo_http_url.js
@@ -0,0 +1,6 @@
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  res.write(req.url);
+  res.end();
+}).listen(8080);
